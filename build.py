2932167e45d4b2c02691661dcd5c0f2b2185c826
Graham Hughes <graehu@gmail.com>
2020-08-01 20:20:47 +0100

    Added confply launcher
    ./build.py rc_sample and ./build.py net_physics_sample will now build
    the two samples. No easy way to say ./build.py all but I'll fix that
    at some point.
    
    Might need a way to do CI via this system too.

--- build.py
+++ build.py
@@ -1,46 +1,24 @@
 #!/usr/bin/env python
-import os
+#                      _____       .__         
+#   ____  ____   _____/ ____\_____ |  | ___.__.
+# _/ ___\/  _ \ /    \   __\____ \|  |<   |  |
+# \  \__(  <_> )   |  \  |  |  |_> >  |_\___  |
+#  \___  >____/|___|  /__|  |   __/|____/ ____|
+#      \/           \/      |__|        \/
+# launcher generated using:
+# 
+# python tools/confply/confply.py --launcher build.py
+
 import sys
-# sys.path.append("tools/confply/")
-# import confply.log as log
+sys.path.append("tools/confply")
+from confply import launcher
 
-# print(os.get_terminal_size())
-
-# requires / ending
-projects = [
-    "src/application/rc_sample/",
-    "src/application/net_physics_sample/"
-]
+# fill this with your commands
+aliases = {
+    #'mycommand':'path/to/command.py'
+    "rc_sample":"src/application/rc_sample/build.py",
+    "net_physics_sample":"src/application/net_physics_sample/build.py"
+}
 
 if __name__ == "__main__":
-
-    in_args = sys.argv[1:]
-    fails = 0
-    if len(in_args) != 0:
-        for arg in in_args:
-            project_found = False
-            for project in projects:
-                project_name = project.split("/")[-2]
-                if project_name == arg:
-                    if os.path.isfile(project+"build.py"):
-                        project_found = True
-                        if os.system("cd "+project+"; ./build.py") != 0:
-                            fails += 1
-                    else:
-                        print("Couldn't find build.py under "+project)
-                        
-            if project_found == False:
-                print("Couldn't find project: "+arg)
-    else:
-        for project in projects:
-            if os.path.isfile(project+"build.py"):
-                if os.system("cd "+project+"; ./build.py") != 0:
-                    fails += 1
-                    
-            else:
-                print("Couldn't find build.py under "+project)
-
-    if fails == 0:
-        print("all projects compile")
-    else:
-        print("some projects fail")
+    launcher(sys.argv[1:], aliases)
