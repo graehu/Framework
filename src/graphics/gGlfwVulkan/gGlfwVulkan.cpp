93d103fd11f14fb947a5a9a773430ce895cb8ae6
Graham Hughes <graehu@gmail.com>
2024-10-12 22:52:21 +0100

    added renderpass
    yup.

--- src/graphics/gGlfwVulkan/gGlfwVulkan.cpp
+++ src/graphics/gGlfwVulkan/gGlfwVulkan.cpp
@@ -26,6 +26,8 @@ namespace fwvulkan
    VkExtent2D g_swap_chain_extent;
    std::vector<VkImage> g_swap_chain_images;
    std::vector<VkImageView> g_swap_chain_image_views;
+   // renderpass
+   VkRenderPass g_render_pass;
    // 
    bool g_enable_validation_layers = false;
    const std::vector<const char*> g_instance_extensions = {
@@ -465,7 +467,7 @@ namespace fwvulkan
 	 // {
 	 //    vkDestroyFramebuffer(logical_device, framebuffer, nullptr);
 	 // }
-	 // vkDestroyRenderPass(logical_device, render_pass, nullptr);
+	 vkDestroyRenderPass(g_logical_device, g_render_pass, nullptr);
 	 // vkDestroyPipeline(logical_device, graphics_pipeline, nullptr);
 	 // vkDestroyPipelineLayout(logical_device, pipeline_layout, nullptr);
 	 for (auto image_view : g_swap_chain_image_views)
@@ -611,6 +613,52 @@ namespace fwvulkan
 	 g_swap_chain_extent = extent;
       }
    }
+   namespace renderpass
+   {
+      void CreateRenderPass()
+      {
+	 std::cout << "CreateRenderPass" << std::endl;
+	 VkAttachmentDescription color_attachement = {};
+	 color_attachement.format = g_swap_chain_image_format;
+	 color_attachement.samples = VK_SAMPLE_COUNT_1_BIT;
+	 color_attachement.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+	 color_attachement.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+	 color_attachement.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+	 color_attachement.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+	 color_attachement.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+	 color_attachement.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+
+	 VkAttachmentReference color_attachemnt_reference = {};
+	 color_attachemnt_reference.attachment = 0;
+	 color_attachemnt_reference.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+
+	 VkSubpassDescription subpass_description = {};
+	 subpass_description.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+	 subpass_description.colorAttachmentCount = 1;
+	 subpass_description.pColorAttachments = &color_attachemnt_reference;
+
+	 VkSubpassDependency subpass_dependency = {};
+	 subpass_dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
+	 subpass_dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	 subpass_dependency.dstSubpass = 0;
+	 subpass_dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+	 subpass_dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+
+	 VkRenderPassCreateInfo render_pass_create_info = {};
+	 render_pass_create_info.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+	 render_pass_create_info.attachmentCount = 1;
+	 render_pass_create_info.pAttachments = &color_attachement;
+	 render_pass_create_info.subpassCount = 1;
+	 render_pass_create_info.pSubpasses = &subpass_description;
+	 render_pass_create_info.dependencyCount = 1;
+	 render_pass_create_info.pDependencies = &subpass_dependency;
+
+	 if (vkCreateRenderPass(g_logical_device, &render_pass_create_info, nullptr, &g_render_pass) != VK_SUCCESS)
+	 {
+	    throw std::runtime_error("failed to create render pass!");
+	 }
+      }
+   }
 }
 int gGlfwVulkan::init()
 {
@@ -632,7 +680,7 @@ int gGlfwVulkan::init()
    fwvulkan::device::CreateLogicalDevice();
    fwvulkan::swapchain::CreateSwapChain();
    fwvulkan::swapchain::CreateSwapchainImageViews();
-   // CreateRenderPass();
+   fwvulkan::renderpass::CreateRenderPass();
    // CreateGraphicsPipeline();
    // CreateFrameBuffers();
    // CreateCommandPool();
