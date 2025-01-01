f53b051c89337c5e6a9632c834af1a71c7b8b13a
Graham Hughes <graehu@gmail.com>
2025-01-01 16:16:51 +0000

    tidying maths
    boring changes.

--- src/types/mat4x4f.cpp
+++ src/types/mat4x4f.cpp
@@ -1,6 +1,5 @@
 #include <memory.h>
 #include <stdio.h>
-//#include "maths.h"
 #include "mat4x4f.h"
 
 const mat4x4f mat4x4f::IDENTITY(1.0f,0.0f,0.0f,0.0f,
@@ -14,136 +13,146 @@ const mat4x4f mat4x4f::NULLMATRIX(0.0f,0.0f,0.0f,0.0f,
 				  0.0f,0.0f,0.0f,0.0f);
 
 
-mat4x4f::~mat4x4f(void)
-{
-}
 
 mat4x4f::mat4x4f(const mat4x4f & rhs)
 {
-	memcpy(elem, rhs.elem, sizeof(float) * 16);
+   memcpy(elem, rhs.elem, sizeof(float) * 16);
 }
 
 mat4x4f::mat4x4f(float _11, float _12, float _13, float _14,
-					 float _21, float _22, float _23, float _24,
-					 float _31, float _32, float _33, float _34,
-					 float _41, float _42, float _43, float _44)
+		 float _21, float _22, float _23, float _24,
+		 float _31, float _32, float _33, float _34,
+		 float _41, float _42, float _43, float _44)
 {
-	elem[0][0] = _11;	elem[0][1] = _12;	elem[0][2] = _13;	elem[0][3] = _14;
-	elem[1][0] = _21;	elem[1][1] = _22;	elem[1][2] = _23;	elem[1][3] = _24;
-	elem[2][0] = _31;	elem[2][1] = _32;	elem[2][2] = _33;	elem[2][3] = _34;
-	elem[3][0] = _41;	elem[3][1] = _42;	elem[3][2] = _43;	elem[3][3] = _44;
+   elem[0][0] = _11;	elem[0][1] = _12;	elem[0][2] = _13;	elem[0][3] = _14;
+   elem[1][0] = _21;	elem[1][1] = _22;	elem[1][2] = _23;	elem[1][3] = _24;
+   elem[2][0] = _31;	elem[2][1] = _32;	elem[2][2] = _33;	elem[2][3] = _34;
+   elem[3][0] = _41;	elem[3][1] = _42;	elem[3][2] = _43;	elem[3][3] = _44;
 }
 
 mat4x4f::mat4x4f(float _elem[][4])
 {
-	memcpy(elem, _elem, sizeof(float) * 16);
+   memcpy(elem, _elem, sizeof(float) * 16);
 }
+
 mat4x4f::mat4x4f(float newElem[16])
 {
-    memcpy(elem,newElem,sizeof(float) * 16);
-}
-mat4x4f operator*( const mat4x4f &M1,
-                     const mat4x4f &M2)
-{
-    mat4x4f ret;
-
-    for(int i = 0; i < 4; i++)
-    {
-        for(int j = 0; j < 4; j++)
-        {
-            float Value = 0;
-
-            for(int k = 0; k < 4; k++)
-            {
-                Value += M1(i,k) * M2(k,j);
-            }
-
-            ret(i,j) = Value;
-        }
-    }
-
-    return ret;
+   memcpy(elem,newElem,sizeof(float) * 16);
 }
 
-vec3f operator * ( const mat4x4f &M,
-					 const vec3f &V )
+mat4x4f operator*( const mat4x4f &M1, const mat4x4f &M2)
 {
-    vec3f ret;
+   mat4x4f ret;
 
-	ret.i = M(0,0) * V.i + M(1,0) * V.j + M(2,0) * V.k + M(3,0);
-	ret.j = M(0,1) * V.i + M(1,1) * V.j + M(2,1) * V.k + M(3,1);
-	ret.k = M(0,2) * V.i + M(1,2) * V.j + M(2,2) * V.k + M(3,2);
-	float w = M(0,3) * V.i + M(1,3) * V.j + M(2,3) * V.k + M(3,3);
-	ret /= w;
-    return ret;
+   for(int i = 0; i < 4; i++)
+   {
+      for(int j = 0; j < 4; j++)
+      {
+	 float Value = 0;
+
+	 for(int k = 0; k < 4; k++)
+	 {
+	    Value += M1(i,k) * M2(k,j);
+	 }
+
+	 ret(i,j) = Value;
+      }
+   }
+
+   return ret;
+}
+
+vec3f operator * (const mat4x4f &M, const vec3f &V)
+{
+   vec3f ret;
+
+   ret.i = M(0,0) * V.i + M(1,0) * V.j + M(2,0) * V.k + M(3,0);
+   ret.j = M(0,1) * V.i + M(1,1) * V.j + M(2,1) * V.k + M(3,1);
+   ret.k = M(0,2) * V.i + M(1,2) * V.j + M(2,2) * V.k + M(3,2);
+   float w = M(0,3) * V.i + M(1,3) * V.j + M(2,3) * V.k + M(3,3);
+   ret /= w;
+   return ret;
 }
 
 void mat4x4f::perspective(float fovy, float aspect, float zNear, float zFar)
 {
-	float range = tan(deg2rag(fovy / float(2))) * zNear;
-	float left = -range * aspect;
-	float right = range * aspect;
-	float bottom = -range;
-	float top = range;
+   float range = tan(deg2rag(fovy / float(2))) * zNear;
+   float left = -range * aspect;
+   float right = range * aspect;
+   float bottom = -range;
+   float top = range;
 
-	//detail::tmat4x4<valType> Result(valType(0));
-	elem[0][0] = (float(2) * zNear) / (right - left);
-	elem[1][1] = (float(2) * zNear) / (top - bottom);
-	elem[2][2] = - (zFar + zNear) / (zFar - zNear);
-	elem[2][3] = - float(1);
-	elem[3][2] = - (float(2) * zFar * zNear) / (zFar - zNear);
-	//return Result;
+   elem[0][0] = (float(2) * zNear) / (right - left);
+   elem[1][1] = (float(2) * zNear) / (top - bottom);
+   elem[2][2] = - (zFar + zNear) / (zFar - zNear);
+   elem[2][3] = - float(1);
+   elem[3][2] = - (float(2) * zFar * zNear) / (zFar - zNear);
+}
+
+void mat4x4f::transpose()
+{
+   mat4x4f temp = *this;
+   for(int i = 0; i < 4; i++)
+   {
+      for(int ii = 0; ii < 4; ii++)
+      {
+	 elem[i][ii] = temp.elem[ii][i];
+      }
+   }
 }
 
 void mat4x4f::translate(float X, float Y, float Z)
 {
-	memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
+   memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
 
-	elem[3][0] = X;
-	elem[3][1] = Y;
-	elem[3][2] = Z;
+   elem[3][0] = X;
+   elem[3][1] = Y;
+   elem[3][2] = Z;
 }
 
 void mat4x4f::scale(float X, float Y, float Z)
 {
-	memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
+   memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
 
-	elem[0][0] = X;
-	elem[1][1] = Y;
-	elem[2][2] = Z;
+   elem[0][0] = X;
+   elem[1][1] = Y;
+   elem[2][2] = Z;
 }
 
 void mat4x4f::rotateX(float fAngle)
 {
-	memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
-	float c = cos(fAngle);
-	float s = sin(fAngle);
-	elem[1][1] = c;
-	elem[1][2] = s;
-	elem[2][1] = -s;
-	elem[2][2] = c;
+   memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
+   
+   float c = cos(fAngle);
+   float s = sin(fAngle);
+   elem[1][1] = c;
+   elem[1][2] = s;
+   elem[2][1] = -s;
+   elem[2][2] = c;
 }
 
 void mat4x4f::rotateY(float fAngle)
 {
-	memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
-	float c = cos(fAngle);
-	float s = sin(fAngle);
-	elem[0][0] = c;
-	elem[2][0] = s;
-	elem[0][2] = -s;
-	elem[2][2] = c;
+   memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
+   
+   float c = cos(fAngle);
+   float s = sin(fAngle);
+   elem[0][0] = c;
+   elem[2][0] = s;
+   elem[0][2] = -s;
+   elem[2][2] = c;
 }
 
 void mat4x4f::rotateZ(float fAngle)
 {
-	memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
-	float c = cos(fAngle);
-	float s = sin(fAngle);
-	elem[0][0] = c;
-	elem[0][1] = s;
-	elem[1][0] = -s;
-	elem[1][1] = c;
+   memcpy(elem, IDENTITY.elem, sizeof(float) * 16);
+   
+   float c = cos(fAngle);
+   float s = sin(fAngle);
+   elem[0][0] = c;
+   elem[0][1] = s;
+   elem[1][0] = -s;
+   elem[1][1] = c;
 }
 
 void mat4x4f::rotate(float x, float y, float z)
@@ -152,6 +161,7 @@ void mat4x4f::rotate(float x, float y, float z)
    mat4x4f ry; ry.rotateY(y);
    mat4x4f rz; rz.rotateZ(z);
    mat4x4f ret = rx*ry*rz;
+   
    memcpy(elem, ret.elem, sizeof(float) * 16);
 }
 
@@ -159,50 +169,50 @@ mat4x4f mat4x4f::rotated(float x, float y, float z)
 {
    mat4x4f ret; ret.rotate(x, y, z); return ret;
 }
+
 mat4x4f mat4x4f::translated(float x, float y, float z)
 {
    mat4x4f ret; ret.translate(x, y, z); return ret;
 }
+
 mat4x4f mat4x4f::scaled(float x, float y, float z)
 {
    mat4x4f ret; ret.scale(x, y, z); return ret;
 }
 
-
 void mat4x4f::lookAt(const vec3f & vFrom, const vec3f & vTo, const vec3f & vUp)
 {
-	vec3f vZ = normalise(vFrom - vTo);
-	//vZ.Dumpvec3f("vZ");
-	vec3f vX = normalise(vUp.cross_product(vZ));
-	//vX.Dumpvec3f("vX");
-	vec3f vY = vZ.cross_product(vX);
-	//vY.Dumpvec3f("vY");
+   vec3f vZ = normalise(vFrom - vTo);
+   vec3f vX = normalise(vUp.cross_product(vZ));
+   vec3f vY = vZ.cross_product(vX);
 
-    elem[0][0] = vX.i;	elem[0][1] = vY.i;	elem[0][2] = vZ.i;	elem[0][3] = 0;
-	elem[1][0] = vX.j;	elem[1][1] = vY.j;	elem[1][2] = vZ.j;	elem[1][3] = 0;
-	elem[2][0] = vX.k;	elem[2][1] = vY.k;	elem[2][2] = vZ.k;	elem[2][3] = 0;
+   elem[0][0] = vX.i;	elem[0][1] = vY.i;	elem[0][2] = vZ.i;	elem[0][3] = 0;
+   elem[1][0] = vX.j;	elem[1][1] = vY.j;	elem[1][2] = vZ.j;	elem[1][3] = 0;
+   elem[2][0] = vX.k;	elem[2][1] = vY.k;	elem[2][2] = vZ.k;	elem[2][3] = 0;
 
-	elem[3][0] = -vX.dot_product3(vFrom);
-	elem[3][1] = -vY.dot_product3(vFrom);
-	elem[3][2] = -vZ.dot_product3(vFrom);
-	elem[3][3] = 1;
+   elem[3][0] = -vX.dot_product3(vFrom);
+   elem[3][1] = -vY.dot_product3(vFrom);
+   elem[3][2] = -vZ.dot_product3(vFrom);
+   elem[3][3] = 1;
 }
+
 float* mat4x4f::asSingleArray()
 {
-    float* a;
-    a = new float[16];
-    memcpy(a,elem[0],sizeof(float) * 4);
-    memcpy(a+4,elem[1],sizeof(float) * 4);
-    memcpy(a+8,elem[2],sizeof(float) * 4);
-    memcpy(a+12,elem[3],sizeof(float) * 4);
-    return a;
+   float* a;
+   a = new float[16];
+   memcpy(a,elem[0],sizeof(float) * 4);
+   memcpy(a+4,elem[1],sizeof(float) * 4);
+   memcpy(a+8,elem[2],sizeof(float) * 4);
+   memcpy(a+12,elem[3],sizeof(float) * 4);
+   return a;
 }
+
 void mat4x4f::dumpMat4x4f(char * s)
 {
-	if(s != NULL)printf("\n%s\n",s);
-	else printf("\n");
-	printf("%f %f %f %f\n",   elem[0][0], elem[0][1], elem[0][2], elem[0][3]);
-	printf("%f %f %f %f\n",   elem[1][0], elem[1][1], elem[1][2], elem[1][3]);
-	printf("%f %f %f %f\n",   elem[2][0], elem[2][1], elem[2][2], elem[2][3]);
-	printf("%f %f %f %f\n\n", elem[3][0], elem[3][1], elem[3][2], elem[3][3]);
+   if(s != NULL)printf("\n%s\n",s);
+   else printf("\n");
+   printf("%f %f %f %f\n",   elem[0][0], elem[0][1], elem[0][2], elem[0][3]);
+   printf("%f %f %f %f\n",   elem[1][0], elem[1][1], elem[1][2], elem[1][3]);
+   printf("%f %f %f %f\n",   elem[2][0], elem[2][1], elem[2][2], elem[2][3]);
+   printf("%f %f %f %f\n\n", elem[3][0], elem[3][1], elem[3][2], elem[3][3]);
 }
--- src/types/mat4x4f.h
+++ src/types/mat4x4f.h
@@ -1,5 +1,4 @@
-#ifndef MAT4X4F_H
-#define MAT4X4F_H
+#pragma once
 
 // #TODO: This file does not follow current conventions. Fix this.
 
@@ -8,118 +7,101 @@
 class mat4x4f
 {
 public:
-	//mat4x4f(void);
-	mat4x4f(const mat4x4f & rhs = IDENTITY);
-	mat4x4f(float _elem[][4]);
-	mat4x4f(float _11, float _12, float _13, float _14,
-				float _21, float _22, float _23, float _24,
-				float _31, float _32, float _33, float _34,
-				float _41, float _42, float _43, float _44);
-    mat4x4f(float newElem[16]); // OpenGL - glGetMatrix
+   mat4x4f(const mat4x4f & rhs = IDENTITY);
+   mat4x4f(float _elem[][4]);
+   mat4x4f(float _11, float _12, float _13, float _14,
+	   float _21, float _22, float _23, float _24,
+	   float _31, float _32, float _33, float _34,
+	   float _41, float _42, float _43, float _44);
+   mat4x4f(float newElem[16]); // OpenGL - glGetMatrix
+ 
+   void perspective(float fovy, float aspect, float zNear, float zFar);
+   void transpose();
 
-	~mat4x4f(void);
-
-	void perspective(float fovy, float aspect, float zNear, float zFar);
-
-	void translate(float X, float Y, float Z);
-	void scale(float X, float Y, float Z);
-	void rotate(float _x, float _y, float z);
-	void rotateX(float fAngle);
-	void rotateY(float fAngle);
-	void rotateZ(float fAngle);
+   void translate(float X, float Y, float Z);
+   void scale(float X, float Y, float Z);
+   void rotate(float _x, float _y, float z);
+   void rotateX(float fAngle);
+   void rotateY(float fAngle);
+   void rotateZ(float fAngle);
 
    static mat4x4f rotated(float x, float y, float z);
    static mat4x4f translated(float x, float y, float z);
    static mat4x4f scaled(float x, float y, float z);
 
-	void dumpMat4x4f(char * s = NULL);
+   void dumpMat4x4f(char * s = NULL);
 
-	void lookAt(const vec3f & vFrom,
-				const vec3f & vTo,
-				const vec3f & vUp);
+   void lookAt(const vec3f & vFrom,
+	       const vec3f & vTo,
+	       const vec3f & vUp);
 
-	inline float &operator()( unsigned int Row, unsigned int Column )
-	{ return elem[Row][Column]; }
-	inline float const &operator()( unsigned int Row, unsigned int Column ) const
-	{ return elem[Row][Column]; }
-	float* asSingleArray(); // WARNING! Delete the memory returned from this function!
+   inline float &operator()( unsigned int Row, unsigned int Column ) { return elem[Row][Column]; }
+   inline float const &operator()( unsigned int Row, unsigned int Column ) const { return elem[Row][Column]; }
+   float* asSingleArray(); // WARNING! Delete the memory returned from this function!
 
-	float elem[4][4];
-	static const mat4x4f IDENTITY;
-	static const mat4x4f NULLMATRIX;
+   float elem[4][4];
+   static const mat4x4f IDENTITY;
+   static const mat4x4f NULLMATRIX;
 };
 
-
-
-mat4x4f operator * ( const mat4x4f & M1,
-					   const mat4x4f & M2 );
-
-vec3f operator * ( const mat4x4f & M,
-					 const vec3f & V );
+mat4x4f operator * ( const mat4x4f & M1, const mat4x4f & M2 );
+vec3f operator * ( const mat4x4f & M, const vec3f & V );
 
 inline mat4x4f operator - ( const mat4x4f & M )
 {
-	return mat4x4f(-M(0,0),-M(0,1),-M(0,2),-M(0,3),
-					 -M(1,0),-M(1,1),-M(1,2),-M(1,3),
-					 -M(2,0),-M(2,1),-M(2,2),-M(2,3),
-					 -M(3,0),-M(3,1),-M(3,2),-M(3,3));
+   return mat4x4f(-M(0,0),-M(0,1),-M(0,2),-M(0,3),
+		  -M(1,0),-M(1,1),-M(1,2),-M(1,3),
+		  -M(2,0),-M(2,1),-M(2,2),-M(2,3),
+		  -M(3,0),-M(3,1),-M(3,2),-M(3,3));
 }
 
-inline mat4x4f operator - ( const mat4x4f & M1,
-							  const mat4x4f & M2 )
+inline mat4x4f operator - ( const mat4x4f & M1, const mat4x4f & M2 )
 {
-	return mat4x4f(M1(0,0)-M2(0,0),M1(0,1)-M2(0,1),M1(0,2)-M2(0,2),M1(0,3)-M2(0,3),
-					 M1(1,0)-M2(1,0),M1(1,1)-M2(1,1),M1(1,2)-M2(1,2),M1(1,3)-M2(1,3),
-					 M1(2,0)-M2(2,0),M1(2,1)-M2(2,1),M1(2,2)-M2(2,2),M1(2,3)-M2(2,3),
-					 M1(3,0)-M2(3,0),M1(3,1)-M2(3,1),M1(3,2)-M2(3,2),M1(3,3)-M2(3,3));
+   return mat4x4f(M1(0,0)-M2(0,0),M1(0,1)-M2(0,1),M1(0,2)-M2(0,2),M1(0,3)-M2(0,3),
+		  M1(1,0)-M2(1,0),M1(1,1)-M2(1,1),M1(1,2)-M2(1,2),M1(1,3)-M2(1,3),
+		  M1(2,0)-M2(2,0),M1(2,1)-M2(2,1),M1(2,2)-M2(2,2),M1(2,3)-M2(2,3),
+		  M1(3,0)-M2(3,0),M1(3,1)-M2(3,1),M1(3,2)-M2(3,2),M1(3,3)-M2(3,3));
 }
 
-inline mat4x4f operator + ( const mat4x4f & M1,
-							  const mat4x4f & M2 )
+inline mat4x4f operator + ( const mat4x4f & M1, const mat4x4f & M2 )
 {
-	return mat4x4f(M1(0,0)+M2(0,0),M1(0,1)+M2(0,1),M1(0,2)+M2(0,2),M1(0,3)+M2(0,3),
-					 M1(1,0)+M2(1,0),M1(1,1)+M2(1,1),M1(1,2)+M2(1,2),M1(1,3)+M2(1,3),
-					 M1(2,0)+M2(2,0),M1(2,1)+M2(2,1),M1(2,2)+M2(2,2),M1(2,3)+M2(2,3),
-					 M1(3,0)+M2(3,0),M1(3,1)+M2(3,1),M1(3,2)+M2(3,2),M1(3,3)+M2(3,3));
+   return mat4x4f(M1(0,0)+M2(0,0),M1(0,1)+M2(0,1),M1(0,2)+M2(0,2),M1(0,3)+M2(0,3),
+		  M1(1,0)+M2(1,0),M1(1,1)+M2(1,1),M1(1,2)+M2(1,2),M1(1,3)+M2(1,3),
+		  M1(2,0)+M2(2,0),M1(2,1)+M2(2,1),M1(2,2)+M2(2,2),M1(2,3)+M2(2,3),
+		  M1(3,0)+M2(3,0),M1(3,1)+M2(3,1),M1(3,2)+M2(3,2),M1(3,3)+M2(3,3));
 }
 
-inline mat4x4f operator * ( const mat4x4f & M,
-							  const float & s )
+inline mat4x4f operator * ( const mat4x4f & M, const float & s )
 {
-	return mat4x4f(M(0,0) * s,M(0,1) * s,M(0,2) * s,M(0,3) * s,
-					 M(1,0) * s,M(1,1) * s,M(1,2) * s,M(1,3) * s,
-					 M(2,0) * s,M(2,1) * s,M(2,2) * s,M(2,3) * s,
-					 M(3,0) * s,M(3,1) * s,M(3,2) * s,M(3,3) * s);
+   return mat4x4f(M(0,0) * s,M(0,1) * s,M(0,2) * s,M(0,3) * s,
+		  M(1,0) * s,M(1,1) * s,M(1,2) * s,M(1,3) * s,
+		  M(2,0) * s,M(2,1) * s,M(2,2) * s,M(2,3) * s,
+		  M(3,0) * s,M(3,1) * s,M(3,2) * s,M(3,3) * s);
 }
 
-inline mat4x4f operator * ( const float & s,
-							  const mat4x4f & M )
+inline mat4x4f operator * ( const float & s, const mat4x4f & M )
 {
-	return mat4x4f(M(0,0) * s,M(0,1) * s,M(0,2) * s,M(0,3) * s,
-					 M(1,0) * s,M(1,1) * s,M(1,2) * s,M(1,3) * s,
-					 M(2,0) * s,M(2,1) * s,M(2,2) * s,M(2,3) * s,
-					 M(3,0) * s,M(3,1) * s,M(3,2) * s,M(3,3) * s);
+   return mat4x4f(M(0,0) * s,M(0,1) * s,M(0,2) * s,M(0,3) * s,
+		  M(1,0) * s,M(1,1) * s,M(1,2) * s,M(1,3) * s,
+		  M(2,0) * s,M(2,1) * s,M(2,2) * s,M(2,3) * s,
+		  M(3,0) * s,M(3,1) * s,M(3,2) * s,M(3,3) * s);
 }
 
 inline float deg2rag(float const degrees)
 {
-        const float pi = float(3.1415926535897932384626433832795);
-		return degrees * (pi / float(180));
+   const float pi = float(3.1415926535897932384626433832795);
+   return degrees * (pi / float(180));
 }
 
-inline mat4x4f Transpose( mat4x4f const & M )
+inline mat4x4f transposed( mat4x4f const & M )
 {
-	mat4x4f ret;
-
-	for(int j = 0; j < 4; j++)
-	{
-		for(int i = 0; i < 4; i++)
-		{
-			ret(i,j) = M(j,i);
-		}
-	}
-
-	return ret;
+   mat4x4f ret;
+   for(int j = 0; j < 4; j++)
+   {
+      for(int i = 0; i < 4; i++)
+      {
+	 ret(i,j) = M(j,i);
+      }
+   }
+   return ret;
 }
-
-#endif//MAT4X4F_H
