bb406e3740fb7d32dff7907a7b5a388e0bd26660
Graham Hughes <graehu@gmail.com>
2025-07-28 22:41:08 +0100

    reorg blob
    more tidying.

--- src/utils/blob.h
+++ src/utils/blob.h
@@ -32,65 +32,13 @@ namespace fw
 	public:
 	 void init(size_t in_capacity, size_t in_page);
 	 bool is_initialised();
-	 void shutdown();
-	 
-	 template<typename T> inline bool save(const char* in_filename, T in_buffer)
-	 {
-	    FILE* file = fopen(in_filename, "wb");
-	    if (file == nullptr) { return false; }
-	    assert(in_buffer.head.ver == fourcc);
-	    if(in_buffer.head.hash == 0)
-	    {
-	       in_buffer.head.hash = hash::hash_buffer((const char*)in_buffer.data, sizeof(*in_buffer.data)*in_buffer.len);
-	    }
-	    fwrite(&in_buffer.head, sizeof(in_buffer.head), 1, file);
-	    fwrite(in_buffer.data, sizeof(*in_buffer.data), in_buffer.len, file);
-	    fclose(file);
-	    return true;
-	 }
-	 template<typename T> inline bool load(const char* in_filename, T& out_buffer)
-	 {
-	    FILE* file = fopen(in_filename, "rb");
-	    if (file == nullptr) { return false; }
-	    fseek(file, 0, SEEK_END);
-	    out_buffer.len = ftell(file);
-	    assert(((out_buffer.len-sizeof(out_buffer.head)) % sizeof(*out_buffer.data)) == 0);
-	    fseek(file, 0, SEEK_SET);
-	    Allocation* alloc = allocate(out_buffer.len + 1);
-	    out_buffer.data = (decltype(out_buffer.data))alloc->data;
-	    // todo: consider a short read
-	    fread((char*)out_buffer.data, out_buffer.len, 1, file);
-	    out_buffer.head = *((header*)out_buffer.data);
-	    alloc->head = *((header*)out_buffer.data);
-	    out_buffer.data = (decltype(out_buffer.data)) ((char*)out_buffer.data+sizeof(out_buffer.head));
-	    assert(out_buffer.head.ver == fourcc);
-	    // todo: decide if a file with no content is ok: (out_buffer.len == sizeof(out_buffer.head)).
-	    assert(out_buffer.head.hash != 0 || out_buffer.len == sizeof(out_buffer.head));
-	    fclose(file);
-	    out_buffer.len = (out_buffer.len-sizeof(out_buffer.head)) / sizeof(*out_buffer.data);
-	    return true;
-	 }
-	 template<typename T> bool free(Buffer<T>& in)
-	 {
-	    if(free((char*)in.data)-sizeof(in.head)) {in = {}; return true;}
-	    return false;
-	 }
-	 template<typename T> bool find(hash::u32 in_hash, Buffer<T>& out_buffer)
-	 {
-	    AllocNode* alloc = used;
-	    while(alloc != nullptr)
-	    {
-	       if(alloc->alloc.head.hash == in_hash)
-	       {
-		  out_buffer = *((Buffer<T>*)&alloc->alloc);
-		  out_buffer.len = (out_buffer.len-sizeof(out_buffer.head)) / sizeof(*out_buffer.data);
-		  return true;
-	       }
-	       alloc = alloc->next;
-	    }
-	    return false;
-	 }
-	 template<typename T> bool fixup(Buffer<T>& out_buffer) { return find(out_buffer.head.hash, out_buffer); }
+	 void shutdown(); 
+	 template<typename T> inline bool save(const char* in_filename, T in_buffer);
+	 template<typename T> inline bool load(const char* in_filename, T& out_buffer);
+	 template<typename T> inline bool free(Buffer<T>& in);
+	 template<typename T> inline bool find(hash::u32 in_hash, Buffer<T>& out_buffer);
+	 template<typename T> inline bool fixup(Buffer<T>& out_buffer);
+
 	private:
 	 Allocation* allocate(size_t);
 	 bool free(char*);
@@ -103,6 +51,63 @@ namespace fw
 	 AllocNode* used = nullptr;
 	 size_t total_allocations = 0;
       };
+      template<typename T> inline bool bank::save(const char* in_filename, T in_buffer)
+      {
+	 FILE* file = fopen(in_filename, "wb");
+	 if (file == nullptr) { return false; }
+	 assert(in_buffer.head.ver == fourcc);
+	 if(in_buffer.head.hash == 0)
+	 {
+	    in_buffer.head.hash = hash::hash_buffer((const char*)in_buffer.data, sizeof(*in_buffer.data)*in_buffer.len);
+	 }
+	 fwrite(&in_buffer.head, sizeof(in_buffer.head), 1, file);
+	 fwrite(in_buffer.data, sizeof(*in_buffer.data), in_buffer.len, file);
+	 fclose(file);
+	 return true;
+      }
+      template<typename T> inline bool bank::load(const char* in_filename, T& out_buffer)
+      {
+	 FILE* file = fopen(in_filename, "rb");
+	 if (file == nullptr) { return false; }
+	 fseek(file, 0, SEEK_END);
+	 out_buffer.len = ftell(file);
+	 assert(((out_buffer.len-sizeof(out_buffer.head)) % sizeof(*out_buffer.data)) == 0);
+	 fseek(file, 0, SEEK_SET);
+	 Allocation* alloc = allocate(out_buffer.len + 1);
+	 out_buffer.data = (decltype(out_buffer.data))alloc->data;
+	 // todo: consider a short read
+	 fread((char*)out_buffer.data, out_buffer.len, 1, file);
+	 out_buffer.head = *((header*)out_buffer.data);
+	 alloc->head = *((header*)out_buffer.data);
+	 out_buffer.data = (decltype(out_buffer.data)) ((char*)out_buffer.data+sizeof(out_buffer.head));
+	 assert(out_buffer.head.ver == fourcc);
+	 // todo: decide if a file with no content is ok: (out_buffer.len == sizeof(out_buffer.head)).
+	 assert(out_buffer.head.hash != 0 || out_buffer.len == sizeof(out_buffer.head));
+	 fclose(file);
+	 out_buffer.len = (out_buffer.len-sizeof(out_buffer.head)) / sizeof(*out_buffer.data);
+	 return true;
+      }
+      template<typename T> inline bool bank::free(Buffer<T>& in)
+      {
+	 if(free((char*)in.data)-sizeof(in.head)) {in = {}; return true;}
+	 return false;
+      }
+      template<typename T> inline bool bank::find(hash::u32 in_hash, Buffer<T>& out_buffer)
+      {
+	 AllocNode* alloc = used;
+	 while(alloc != nullptr)
+	 {
+	    if(alloc->alloc.head.hash == in_hash)
+	    {
+	       out_buffer = *((Buffer<T>*)&alloc->alloc);
+	       out_buffer.len = (out_buffer.len-sizeof(out_buffer.head)) / sizeof(*out_buffer.data);
+	       return true;
+	    }
+	    alloc = alloc->next;
+	 }
+	 return false;
+      }
+      template<typename T> inline bool bank::fixup(Buffer<T>& out_buffer) { return find(out_buffer.head.hash, out_buffer); }
       extern bank miscbank;
    }
 }
